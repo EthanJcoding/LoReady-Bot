c89618d1538f474680ba8ed9bffc2579365a9112
ethanJcoding <ethan.95j@gmail.com>
2024-03-12 17:29:37 +0900

    ✨ 프론트 요구사항 캐릭터 배열 객체로 변환 추가

--- src/api/joinSchedule/joinSchedule.js
+++ src/api/joinSchedule/joinSchedule.js
@@ -1,13 +1,19 @@
-import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { arrayUnion, doc, getDoc, updateDoc } from "firebase/firestore";
 import { firestore } from "../../bot.js";
 
 const joinSchedule = async (scheduleId, userId, character) => {
   const scheduleRef = doc(firestore, "schedules", scheduleId);
+  const scheduleSnap = await getDoc(scheduleRef);
+
+  const { characters } = scheduleSnap.data();
+
+  const updatedCharacters = { ...characters };
 
   try {
+    updatedCharacters.party0.push({ character, userId });
     updateDoc(scheduleRef, {
       participants: arrayUnion(userId),
-      characters: arrayUnion({ character, userId }),
+      characters: updatedCharacters,
     });
   } catch (err) {
     console.log(err);
